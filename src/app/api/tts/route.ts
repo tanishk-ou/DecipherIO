bca3642496f89bcb9e477b039556c4995db1038d
Tanishk <gopalanitanishk@gmail.com>
2026-03-20 19:56:45 +0530

    Fixed TTS

--- src/app/api/tts/route.ts
+++ src/app/api/tts/route.ts
@@ -5,7 +5,10 @@ import { TextToSpeechClient } from "@google-cloud/text-to-speech";
 const client = new TextToSpeechClient({
   credentials: {
     client_email: process.env.GOOGLE_CLIENT_EMAIL,
-    private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    private_key: process.env.GOOGLE_PRIVATE_KEY
+      ?.replace(/\\n/g, '\n')  // Fixes escaped newlines
+      ?.replace(/"/g, '')      // Strips accidental double quotes
+      ?.replace(/'/g, ''),     // Strips accidental single quotes
   },
 });
 
